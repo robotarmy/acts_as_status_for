bf8269b21767225196ba12e767df7651dc6ab733
robotarmy <178963+robotarmy@users.noreply.github.com>
2015-02-10 16:22:48 -0800

    Version bump to 4.1.0

--- spec/acts_as_status_for_spec.rb
+++ spec/acts_as_status_for_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 ActiveRecord::Migration.create_table :things do |t|
   t.string   :name
+  t.string   :type # allow single table inheritance
   t.datetime :on_hold_at
   t.datetime :archived_at
   t.datetime :featured_at
@@ -105,90 +106,119 @@ describe ActsAsStatusFor do
     end
   end
   context "#status" do
-    before do
-      Thing.instance_eval do
-        acts_as_status_for :on_hold, :archived, :featured
+    context "with multi object inheritance" do
+      before do
+        Thing.instance_eval do
+          acts_as_status_for :on_hold, :archived, :featured
+        end
+        class ThingA < Thing
+
+        end
+        class ThingB < ThingA
+
+        end
+      end
+      context "it searches inheritance tree for on_at_methods" do
+        subject {
+          ThingB.new(:name => 'required')
+        }
+        it "defaults to ''" do
+          expect(subject.status).to eq('')
+        end
+        it "uses superclasse status" do
+          subject.on_hold!
+          expect(subject.status).to eq('on_hold')
+        end
       end
     end
 
-    it "defaults to ''" do
-      expect(subject.status).to eq('')
-    end
-
-    it "allows negation of status using 'not_' prefix" do
-      subject.on_hold!
-      subject.archived!
-      expect(subject.status).to eq('archived on_hold')
-      subject.status = "not_archived"
-      expect(subject.status).to eq('on_hold')
-    end
-
-    it "is sorted by event time" do
-      subject.on_hold!
-      expect(subject.status).to eq('on_hold')
-      subject.archived!
-      expect(subject.status).to eq('archived on_hold')
-      subject.featured!
-      expect(subject.status).to eq('featured archived on_hold')
-      subject.not_on_hold!
-      subject.on_hold!
-      expect(subject.status).to eq('on_hold featured archived')
-    end
-
-    it "setting it to blank clears all states" do
-      subject.on_hold!
-      subject.archived!
-      subject.featured!
-      subject.status = ''
-      expect(subject.status).to eq('')
-    end
-
-    describe "audit trail is independent of scope" do
-      it "should recognize only the most recent" do
-        subject.featured_at = 3.days.ago
-        subject.on_hold_at = 1.days.ago
-        subject.archived_at = Time.now
-        subject.save
-        expect(subject.class.archived).to     include(subject)
-        expect(subject.class.not_on_hold).to  include(subject)
-        expect(subject.class.on_hold).not_to  include(subject)
-        expect(subject.class.not_featured).to include(subject)
-        expect(subject.class.featured).not_to include(subject)
+    context "with single object" do
+      before do
+        Thing.instance_eval do
+          acts_as_status_for :on_hold, :archived, :featured
+        end
       end
-    end
 
-    (ClassFinderMethods = {
-      # class finder method 
-      #          => [ list of event states that belong to finder]
-      #
-      :featured  => [:featured],
-      :on_hold   => [:on_hold],
-      :archived  => [:archived]
-    }).each do |scope,states|
-      states.each do |state|
-        it "can be used to set events" do
-          subject.status = state.to_s
-          expect(subject.send(%%#{state}?%)).to be_truthy
-          expect(subject.status).to include(state.to_s)
+      it "defaults to ''" do
+        expect(subject.status).to eq('')
+      end
+
+      it "allows negation of status using 'not_' prefix" do
+        subject.on_hold!
+        subject.archived!
+        expect(subject.status).to eq('archived on_hold')
+        subject.status = "not_archived"
+        expect(subject.status).to eq('on_hold')
+      end
+
+      it "is sorted by event time" do
+        subject.on_hold!
+        expect(subject.status).to eq('on_hold')
+        subject.archived!
+        expect(subject.status).to eq('archived on_hold')
+        subject.featured!
+        expect(subject.status).to eq('featured archived on_hold')
+        subject.not_on_hold!
+        subject.on_hold!
+        expect(subject.status).to eq('on_hold featured archived')
+      end
+
+      it "setting it to blank clears all states" do
+        subject.on_hold!
+        subject.archived!
+        subject.featured!
+        subject.status = ''
+        expect(subject.status).to eq('')
+      end
+
+
+      describe "audit trail is independent of scope" do
+        it "should recognize only the most recent" do
+          subject.featured_at = 3.days.ago
+          subject.on_hold_at = 1.days.ago
+          subject.archived_at = Time.now
+          subject.save
+          expect(subject.class.archived).to     include(subject)
+          expect(subject.class.not_on_hold).to  include(subject)
+          expect(subject.class.on_hold).not_to  include(subject)
+          expect(subject.class.not_featured).to include(subject)
+          expect(subject.class.featured).not_to include(subject)
         end
+      end
 
-        it "can be reversed" do
-          subject.status = state.to_s
-          expect(subject.send("#{state}?")).to be_truthy
-          subject.status = "not_" + state.to_s
-          expect(subject.send("#{state}?")).to be_falsey
-        end
+      (ClassFinderMethods = {
+        # class finder method 
+        #          => [ list of event states that belong to finder]
+        #
+        :featured  => [:featured],
+        :on_hold   => [:on_hold],
+        :archived  => [:archived]
+      }).each do |scope,states|
+        states.each do |state|
+          it "can be used to set events" do
+            subject.status = state.to_s
+            expect(subject.send(%%#{state}?%)).to be_truthy
+            expect(subject.status).to include(state.to_s)
+          end
 
-        it "#{state} sets state string" do
-          subject.send("#{state}!")
-          expect(subject.send("#{state}?")).to be_truthy
-          expect(subject.status).to include(state.to_s)
-        end
+          it "can be reversed" do
+            subject.status = state.to_s
+            expect(subject.send("#{state}?")).to be_truthy
+            subject.status = "not_" + state.to_s
+            expect(subject.send("#{state}?")).to be_falsey
+          end
 
-        it "#{state} is in the scope #{scope}" do
-          subject.send("#{state}!")
-          expect(subject.send("#{state}?")).to be_truthy
-          expect(subject.class.send(scope)).to include(subject)
+          it "#{state} sets state string" do
+            subject.send("#{state}!")
+            expect(subject.send("#{state}?")).to be_truthy
+            expect(subject.status).to include(state.to_s)
+          end
+
+          it "#{state} is in the scope #{scope}" do
+            subject.send("#{state}!")
+            expect(subject.send("#{state}?")).to be_truthy
+            expect(subject.class.send(scope)).to include(subject)
+          end
         end
       end
     end
